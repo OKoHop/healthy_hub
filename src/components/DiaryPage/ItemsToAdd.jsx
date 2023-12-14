da920fa20ee01b4b731999643bd4e4efd25ee635
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-12-14 23:15:15 +0100

    created selectors & components (elementOfFood & itemsToAdd) + styled components

---
+++ src/components/DiaryPage/ItemsToAdd.jsx
@@ -0,0 +1,30 @@
+import {
+    Info,
+    Value, 
+    CarbonohidratesWrap,
+    ProteinWrap,
+    FatWrap,
+  } from '../DiaryPageItemsToAdd/ItemsToAdd.styled';
+  
+  const ItemToAdd = ({ info }) => {
+  
+    const { carbonohidrates, protein, fat } = info;
+  
+    return (      
+      <Info>
+        <CarbonohidratesWrap>
+          Carbonohidrates: <Value>{carbonohidrates}</Value>
+        </CarbonohidratesWrap>
+        <ProteinWrap>
+          Protein: <Value>{protein}</Value>
+        </ProteinWrap>
+        <FatWrap>
+          Fat: <Value>{fat}</Value>
+        </FatWrap>
+      </Info>
+      
+    );
+  };
+  
+  export default ItemToAdd;
+  
