4d8504b238de231c15fd148066c9682668c192f6
Nikita Kozin <87591503+OKoHop@users.noreply.github.com>
2023-12-14 23:22:55 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 
 import { GlobalStyle } from './GlobalStyles';
@@ -9,12 +8,12 @@ import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
+   
       <BrowserRouter basename="/healthy_hub">
         <App />
         <GlobalStyle />
         <Toaster position="top-center" reverseOrder={false} />
       </BrowserRouter>
-    </Provider>
+    
   </React.StrictMode>
 );
