375e5ff805b8e431e45e56a9823ae3eb855ac04f
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-12-16 22:58:51 +0200

    add store

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import 'dotenv/config';
 
 axios.defaults.baseURL = process.env.DB_URI;
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { authReducer } from './auth/slice';
+// import { authReducer } from './auth/slice';
 import { foodsReducer } from './foods/foodsSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    // auth: authReducer,
     foods: foodsReducer,
   },
 });
