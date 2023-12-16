565fad9280121a865af533e6a3ae7931889f1237
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-12-17 01:14:06 +0200

    add all routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,17 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Welcome />} />
           <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" />
+          <Route path="forgot-password" />
           <Route path="/main" element={<Today />} />
+          <Route path="/dashboard" />
+          <Route path="/diary" />
+          <Route path="recommended-food" />
+          <Route path="/settings" />
+          <Route path="*" element={<Layout />} />
         </Route>
       </Routes>
     </>
-
   );
 }
 export default App;
