3cba42e16ed21cc028abe3a2c1d5c8c4c2d99131
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-12-16 22:50:32 +0200

    add store

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,19 @@ import { Toaster } from 'react-hot-toast';
 import { theme } from './theme.js';
 import { GlobalStyle } from './GlobalStyles';
 import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/healthy_hub">
-        <App />
-        <GlobalStyle />
-        <Toaster position="top-center" reverseOrder={false} />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter basename="/healthy_hub">
+          <App />
+          <GlobalStyle />
+          <Toaster position="top-center" reverseOrder={false} />
+        </BrowserRouter>
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
