28966df2625bf8195d66aeed7953824421d09fae
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-12-20 21:00:38 +0200

    fix privat route

--- src/components/Routes/PrivatRoutes.jsx
+++ src/components/Routes/PrivatRoutes.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import { Navigate } from 'react-router-dom';
 
 export const PrivatRoute = ({ component: Component, redirectTo = '/' }) => {
   const isLogIn = useSelector(selectIsLoggedIn);
