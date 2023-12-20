fa309c7f786490fb0ba4d919075b545780b07fd1
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-12-20 22:00:15 +0200

    Update ElementOfFood.jsx

--- src/components/DiaryPage/ElementOfFood.jsx
+++ src/components/DiaryPage/ElementOfFood.jsx
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream:src/components/DiaryPage/ElementOfFood.jsx
 import edit from '../../images/diaryPageImages/edit-2.svg'
+=======
+import { useState } from 'react';
+import RecordDiaryModal from '../RecordDiaryModal/RecordDiaryModal';
+import UpdateMealModal from '../UpdateMealModal/UpdateMealModal';
+import edit from '../../../images/diaryPageImages/edit-2.svg';
+>>>>>>> Stashed changes:src/components/Diary/Diary/ElementOfFood.jsx
 
 import {
   Wrapper,
@@ -14,6 +21,7 @@ import {
   Fat,
 } from './ElementOfFood.styled';
 
+<<<<<<< Updated upstream:src/components/DiaryPage/ElementOfFood.jsx
 
 const ElementOfFood = ({ item, index }) => {
   const { carbohydrate, protein, fat, mealName, mealType, showButton } = item;  
@@ -58,6 +66,68 @@ const ElementOfFood = ({ item, index }) => {
             )}
           </Wrapper>
         )}
+=======
+const ElementOfFood = ({ item, index, img }) => {
+  const [isRecordModalOpen, setIsRecordModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  const toggleModalRecord = () => setIsRecordModalOpen(!isRecordModalOpen);
+  const toggleModalEdit = () => setIsEditModalOpen(!isEditModalOpen);
+
+  const { carbohydrate, protein, fat, mealName, mealType, showButton } = item;
+
+  return (
+    <>
+      {showButton ? (
+        <Wrapper>
+          <SequenceNumber>{index + 1}</SequenceNumber>
+          <AddFoodButton onClick={toggleModalRecord}>
+            + Record your meal
+          </AddFoodButton>
+          {isRecordModalOpen && (
+            <RecordDiaryModal
+              onClose={toggleModalRecord}
+              image={img}
+              mealType={mealType}
+            />
+          )}
+        </Wrapper>
+      ) : (
+        <Wrapper>
+          <SequenceNumber>{index + 1}</SequenceNumber>
+          {mealType && (
+            <Dish>
+              <Title>{mealName}</Title>
+              <EditButton type="button" onClick={toggleModalEdit}>
+                <Img src={edit} alt="Edit" />
+                Edit
+              </EditButton>
+              <BlockInfo>
+                <Carbonohidrates>
+                  <span>Carb.</span>
+                  {carbohydrate}
+                </Carbonohidrates>
+                <Protein>
+                  <span>Prot.</span>
+                  {protein}
+                </Protein>
+                <Fat>
+                  <span>Fat.</span>
+                  {fat}
+                </Fat>
+              </BlockInfo>
+            </Dish>
+          )}
+          {isEditModalOpen && (
+            <UpdateMealModal
+              onClose={toggleModalEdit}
+              mealType={mealType}
+              item={item}
+            />
+          )}
+        </Wrapper>
+      )}
+>>>>>>> Stashed changes:src/components/Diary/Diary/ElementOfFood.jsx
     </>
   );
 };
