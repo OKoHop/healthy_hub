d7ca61bc8759023b842785ba157342a67a01e443
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-12-22 21:42:51 +0200

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -73,7 +73,7 @@ export const refreshUser = createAsyncThunk(
 
     try {
       setAuthHeader(persistedToken);
-      const res = await axios.get('/users/current');
+      const res = await axios.get('/api/user/current');
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
