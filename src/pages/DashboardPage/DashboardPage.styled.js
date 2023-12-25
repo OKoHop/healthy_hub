1dd08af5eea1c0fe079eb4a33899cb6e153a5592
Romano1994o <135154365+Romano1994o@users.noreply.github.com>
2023-12-25 22:48:30 +0100

    statistics
    Co-Authored-By: Nikita Kozin <87591503+OKoHop@users.noreply.github.com>

---
+++ src/pages/DashboardPage/DashboardPage.styled.js
@@ -0,0 +1,129 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const DashboardSection = styled.section`
+  background-color: #050505;
+  color: #ffffff;
+  font-family: 'Poppins', sans-serif;
+`;
+
+export const DashboardContainer = styled.div`
+  margin: 0 auto;
+  @media screen and (${(props) => props.theme.mq.mobMin}) {
+    width: 320px;
+    padding: 16px 10px 60px;
+  }
+  @media screen and (${(props) => props.theme.mq.tabMin}) {
+    width: 834px;
+    padding: 24px 27px 60px;
+  }
+  @media screen and (${(props) => props.theme.mq.deskMin}) {
+    width: 1440px;
+    padding: 20px 34px 40px;
+  }
+`;
+
+export const HeaderBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 16px;
+`;
+
+export const MainHeaderBlock = styled.div`
+  position: relative;
+  display: flex;
+`;
+
+export const BackLink = styled(NavLink)`
+  display: flex;
+  @media screen and (${(props) => props.theme.mq.mobMin}) {
+    margin-right: 8px;
+  }
+  @media screen and (${(props) => props.theme.mq.tabMin}) {
+    margin-right: 12px;
+  }
+`;
+
+export const ArrowReturn = styled.img`
+  @media screen and (${(props) => props.theme.mq.mobMin}) {
+    width: 16px;
+  }
+  @media screen and (${(props) => props.theme.mq.tabMin}) {
+    width: 24px;
+  }
+`;
+
+
+
+export const Header = styled.h1`
+  font-weight: 500;
+
+  @media screen and (${(props) => props.theme.mq.mobMin}) {
+    font-size: 24px;
+    line-height: 1.25;
+  }
+  @media screen and (${(props) => props.theme.mq.tabMin}) {
+    font-size: 30px;
+    line-height: 1.2;
+  }
+`;
+
+export const SecondHeader = styled.h3`
+  font-weight: 500;
+
+  @media screen and (${(props) => props.theme.mq.mobMin}) {
+    font-size: 16px;
+    line-height: 1.38;
+  }
+  @media screen and (${(props) => props.theme.mq.tabMin}) {
+    font-size: 18px;
+    line-height: 1.33;
+  }
+`;
+
+export const LineChartBlock = styled.div`
+  display: flex;
+  @media screen and (${(props) => props.theme.mq.mobMin}) {
+    flex-direction: column;
+  }
+  @media screen and (${(props) => props.theme.mq.deskMin}) {
+    flex-direction: row;
+    align-items: baseline;
+    justify-content: space-between;
+  }
+`;
+
+export const ChartGrid = styled.div`
+  @media screen and (${(props) => props.theme.mq.mobMin}) {
+    display: flex;
+    flex-direction: column;
+    overflow-x: scroll;
+    &:not(:last-child) {
+      margin-bottom: 27px;
+    }
+  }
+  @media screen and (${(props) => props.theme.mq.tabMin}) {
+    display: imline-block;
+    overflow-x: hidden;
+    &:not(:last-child) {
+      margin-bottom: 40px;
+    }
+  }
+`;
+
+export const ScaleChartBlock = styled.div`
+  @media screen and (${(props) => props.theme.mq.mobMin}) {
+    display: flex;
+    flex-direction: column;
+    overflow-x: scroll;
+    margin-top: 27px;
+  }
+  @media screen and (${(props) => props.theme.mq.tabMin}) {
+    margin-top: 40px;
+    overflow-x: hidden;
+  }
+  @media screen and (${(props) => props.theme.mq.deskMin}) {
+    margin-top: 20px;
+  }
+`;
