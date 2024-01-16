07433fca6094e13066b2c3cc3bc0f2a14d8bb0bf
Danylo Kliushnychenko <danylktv2@gmail.com>
2024-01-16 12:28:08 +0000

    feat: Redux integration for successful forgot password operation

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -2,5 +2,4 @@ export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectUser = (state) => state.auth.user;
 export const selectIsRefreshing = (state) => state.auth.isRefreshing;
 export const selectError = (state) => state.auth.error;
-
-
+export const selectSuccess = (state) => state.auth.success;
